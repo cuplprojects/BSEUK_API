2f9de23da6543114f9956ba869d5ccb0b33eea87
cuplprojects <shivom.chandrakala@gmail.com>
2025-01-04 11:36:36 +0530

    Institutecontrolleradded

---
+++ BSEUK/Models/Institute.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BSEUK.Models
+{
+    public class Institute
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        public string InstituteName { get; set; }
+    }
+}
